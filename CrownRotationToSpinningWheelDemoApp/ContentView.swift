2fa85491d6dcd88d1e449b09f133be00fc0b4ce5
Josh Cook <joshuacook0023@gmail.com>
2021-01-21 07:09:58 -0500

    feat: gesture interaction linked to rotation of circle

--- CrownRotationToSpinningWheelDemoApp/ContentView.swift
+++ CrownRotationToSpinningWheelDemoApp/ContentView.swift
@@ -8,21 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValue: Double = 0.0
+    @State private var gestureValue: CGSize = .zero
+    @State private var damping: Double = 0.0
 
     var body: some View {
-        VStack {
+        let drag = DragGesture()
+            .onChanged { value in
+                self.gestureValue = value.translation
+            }
+            .onEnded { _ in
+                self.gestureValue = .zero
+            }
+
+        return VStack {
             LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
                 .clipShape(Circle())
                 .padding(5)
+                .rotationEffect(.degrees(Double(gestureValue.width)))
+                .animation(.interactiveSpring())
 
-            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                .foregroundColor(.gray)
-                .padding()
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .foregroundColor(.gray)
+                    .gesture(drag)
+
+                VStack {
+                    Spacer()
+                    Text("x: \(gestureValue.width, specifier: "%.2f"), y: \(gestureValue.height, specifier: "%.2f")")
+                        .font(.caption)
+                        .padding(3)
+                }
+            }
+            .padding()
 
             HStack {
                 Image(systemName: "minus")
-                Slider(value: $sliderValue, in: 0 ... 1, step: 0.01).accentColor(.green)
+                Slider(value: $damping, in: 0 ... 1, step: 0.01).accentColor(.green)
                 Image(systemName: "plus")
             }
             .foregroundColor(.green)
@@ -34,7 +55,7 @@ struct ContentView: View {
             )
             .padding()
 
-            Text("Damping: \(sliderValue, specifier: "%.2f")")
+            Text("Damping: \(damping, specifier: "%.2f")")
                 .padding(.top, 0)
                 .padding(.bottom)
         }
