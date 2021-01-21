c90feb74d5b8a96d8c33ea9f2a9cf39be3d503b8
Josh Cook <joshuacook0023@gmail.com>
2021-01-21 06:48:03 -0500

    feat: basic UI

--- CrownRotationToSpinningWheelDemoApp/ContentView.swift
+++ CrownRotationToSpinningWheelDemoApp/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Double = 0.0
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                .clipShape(Circle())
+                .padding(5)
+
+            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                .foregroundColor(.gray)
+                .padding()
+
+            HStack {
+                Image(systemName: "minus")
+                Slider(value: $sliderValue, in: 0 ... 1, step: 0.01).accentColor(.green)
+                Image(systemName: "plus")
+            }
+            .foregroundColor(.green)
             .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .stroke(lineWidth: 2.0)
+                    .foregroundColor(.green)
+            )
+            .padding()
+
+            Text("Damping: \(sliderValue, specifier: "%.2f")")
+                .padding(.top, 0)
+                .padding(.bottom)
+        }
     }
 }
 
