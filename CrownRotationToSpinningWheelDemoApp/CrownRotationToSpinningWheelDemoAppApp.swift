107307a551d68d335de2375b6d499245cebb78fb
Josh Cook <joshuacook0023@gmail.com>
2021-01-21 06:16:15 -0500

    Initial Commit

---
+++ CrownRotationToSpinningWheelDemoApp/CrownRotationToSpinningWheelDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrownRotationToSpinningWheelDemoAppApp.swift
+//  CrownRotationToSpinningWheelDemoApp
+//
+//  Created by Joshua on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CrownRotationToSpinningWheelDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
